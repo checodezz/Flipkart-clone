4e1e5589894ab2c26ca07698364498f0882d29d4
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-09-21 07:39:38 +0530

    feat : add banner to homepage

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,7 +1,21 @@
 import Navbar from "./Navbar";
+import Banner from "./Banner";
+import { Box, styled } from "@mui/material";
+
+const Component = styled(Box)`
+  padding: 10px;
+  background: #f2f2f2;
+`;
 
 const Home = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Component>
+        <Banner />
+      </Component>
+    </>
+  );
 };
 
 export default Home;
