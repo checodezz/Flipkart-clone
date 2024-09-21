f6b6f33cfcb81bf70b5fd50dce8080cbc067c737
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-09-21 14:07:46 +0530

    add backend with initial data

---
+++ backend/default.js
@@ -0,0 +1,14 @@
+import { products } from "./constants/data.js"
+import Product from "./model/product.schema.js"
+
+const defaultData = async () => {
+    try {
+        // await Product.deleteMany({})
+        await Product.insertMany(products);
+        console.log(`Data imported Successfully`)
+    } catch (error) {
+        console.log("Error while inserting default data", (error))
+    }
+}
+
+export default defaultData 
