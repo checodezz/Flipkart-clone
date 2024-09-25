0fb611eaa40cc229f182633ee8d28aeb14d7c933
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-09-25 11:25:22 +0530

    feat : add get product by id route and api

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,10 +1,10 @@
 import express from "express"
 import { userSignup, userLogin } from "../controller/userController.js";
-import { getProducts } from "../controller/productController.js";
+import { getProducts, getProductById } from "../controller/productController.js";
 const router = express.Router();
 
 router.post("/signup", userSignup);
-router.post("/login", userLogin)
-router.get("/products", getProducts)
-
+router.post("/login", userLogin);
+router.get("/products", getProducts);
+router.get("/product/:id", getProductById)
 export default router;
