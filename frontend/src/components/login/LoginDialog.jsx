d8d62afb42ec6a63a48a50413141f0d08f12bc4b
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-10-07 15:40:32 +0530

    fix : ui

--- frontend/src/components/login/LoginDialog.jsx
+++ frontend/src/components/login/LoginDialog.jsx
@@ -81,108 +81,112 @@ const LoginDialog = ({ open, setOpen }) => {
       onClose={handleClose}
       PaperProps={{ sx: { maxWidth: "unset" } }}
     >
-      <div className="flex h-[65vh] w-[90vh]">
-        <div className="bg-[#2874f0] bg-[url('https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png')] bg-center bg-no-repeat bg-[85%] h-[100%] w-[38%] p-[20px]">
+      <div className="flex h-auto w-full max-w-[90vw] lg:max-w-[70vw] flex-col lg:flex-row">
+        {/* Image section */}
+        <div className="hidden lg:block bg-[#2874f0] bg-[url('https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png')] bg-center bg-no-repeat bg-cover w-[40%] h-auto p-[20px]">
           <h5 className="text-white font-semibold text-xl">
             {account.heading}
           </h5>
           <p className="mt-5 text-white">{account.subHeading}</p>
         </div>
 
-        {account.view === "login" ? (
-          <div className="flex flex-col p-[25px]">
-            <input
-              className="border-b border-gray-400 p-2 focus:outline-none"
-              type="text"
-              placeholder="Enter Username"
-              name="username"
-              onChange={onValueChange}
-            />
-            {error && (
-              <p className="text-[#ff6161] text-xs font-semibold mt-2">
-                Please enter valid username and password.
+        {/* Form section */}
+        <div className="flex flex-col p-[25px] w-full max-w-[350px] lg:max-w-none lg:w-[60%] mx-auto">
+          {account.view === "login" ? (
+            <>
+              <input
+                className="border-b border-gray-400 p-2 focus:outline-none"
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+                onChange={onValueChange}
+              />
+              {error && (
+                <p className="text-[#ff6161] text-xs font-semibold mt-2">
+                  Please enter valid username and password.
+                </p>
+              )}
+              <input
+                className="border-b border-gray-400 p-2 focus:outline-none mt-4"
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+                onChange={onValueChange}
+              />
+              <p className="text-gray-500 text-xs mt-3">
+                By continuing, you agree to Flipkart's Terms of Use and Privacy
+                Policy.
               </p>
-            )}
-            <input
-              className="border-b border-gray-400 p-2 focus:outline-none mt-4"
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              onChange={onValueChange}
-            />
-            <p className="text-gray-500 text-xs mt-3">
-              By continuing, you agree to Flipkart's Terms of Use and Privacy
-              Policy.
-            </p>
-            <button
-              className="bg-[#fb641b] text-white h-[48px] rounded-md mt-4"
-              onClick={loginUser}
-            >
-              Login
-            </button>
-            <p className="text-center mt-4">OR</p>
-            <button className="bg-white text-[#2874f0] h-[48px] rounded-md mt-4 shadow-sm">
-              Request OTP
-            </button>
-            <p
-              className="text-[#2874f0] text-sm font-semibold text-center cursor-pointer mt-4"
-              onClick={toggleSignUp}
-            >
-              New to Flipkart? Create an account
-            </p>
-          </div>
-        ) : (
-          <div className="flex flex-col p-[25px]">
-            <input
-              className="border-b border-gray-400 p-2 focus:outline-none"
-              type="text"
-              placeholder="Enter Firstname"
-              name="firstname"
-              onChange={onInputChange}
-            />
-            <input
-              className="border-b border-gray-400 p-2 focus:outline-none mt-4"
-              type="text"
-              placeholder="Enter Lastname"
-              name="lastname"
-              onChange={onInputChange}
-            />
-            <input
-              className="border-b border-gray-400 p-2 focus:outline-none mt-4"
-              type="text"
-              placeholder="Enter Username"
-              name="username"
-              onChange={onInputChange}
-            />
-            <input
-              className="border-b border-gray-400 p-2 focus:outline-none mt-4"
-              type="email"
-              placeholder="Enter Email"
-              name="email"
-              onChange={onInputChange}
-            />
-            <input
-              className="border-b border-gray-400 p-2 focus:outline-none mt-4"
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              onChange={onInputChange}
-            />
-            <input
-              className="border-b border-gray-400 p-2 focus:outline-none mt-4"
-              type="text"
-              placeholder="Enter Phone"
-              name="phone"
-              onChange={onInputChange}
-            />
-            <button
-              className="bg-[#fb641b] text-white h-[48px] rounded-md mt-4"
-              onClick={signupUser}
-            >
-              Continue
-            </button>
-          </div>
-        )}
+              <button
+                className="bg-[#fb641b] text-white h-[48px] rounded-md mt-4"
+                onClick={loginUser}
+              >
+                Login
+              </button>
+              <p className="text-center mt-4">OR</p>
+              <button className="bg-white text-[#2874f0] h-[48px] rounded-md mt-4 shadow-sm">
+                Request OTP
+              </button>
+              <p
+                className="text-[#2874f0] text-sm font-semibold text-center cursor-pointer mt-4"
+                onClick={toggleSignUp}
+              >
+                New to Flipkart? Create an account
+              </p>
+            </>
+          ) : (
+            <>
+              <input
+                className="border-b border-gray-400 p-2 focus:outline-none"
+                type="text"
+                placeholder="Enter Firstname"
+                name="firstname"
+                onChange={onInputChange}
+              />
+              <input
+                className="border-b border-gray-400 p-2 focus:outline-none mt-4"
+                type="text"
+                placeholder="Enter Lastname"
+                name="lastname"
+                onChange={onInputChange}
+              />
+              <input
+                className="border-b border-gray-400 p-2 focus:outline-none mt-4"
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+                onChange={onInputChange}
+              />
+              <input
+                className="border-b border-gray-400 p-2 focus:outline-none mt-4"
+                type="email"
+                placeholder="Enter Email"
+                name="email"
+                onChange={onInputChange}
+              />
+              <input
+                className="border-b border-gray-400 p-2 focus:outline-none mt-4"
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+                onChange={onInputChange}
+              />
+              <input
+                className="border-b border-gray-400 p-2 focus:outline-none mt-4"
+                type="text"
+                placeholder="Enter Phone"
+                name="phone"
+                onChange={onInputChange}
+              />
+              <button
+                className="bg-[#fb641b] text-white h-[48px] rounded-md mt-4"
+                onClick={signupUser}
+              >
+                Continue
+              </button>
+            </>
+          )}
+        </div>
       </div>
     </Dialog>
   );
