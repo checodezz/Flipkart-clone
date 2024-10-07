dc611c5ea2d5b8b8f185c099e621ca5dad166e38
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-10-07 16:28:17 +0530

    fix : typo

--- frontend/src/constants/data.js
+++ frontend/src/constants/data.js
@@ -5,9 +5,9 @@ export const navData = [
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100', text: 'Laptops' },
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/ee162bad964c46ae.png?q=100', text: 'Home ' },
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100', text: 'Grocery' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100', text: 'Beauty, Toys & More' },
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100', text: 'Appliances' },
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100', text: 'Travel' },
+    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100', text: 'Toys & More' },
 ];
 
 export const bannerData = [
